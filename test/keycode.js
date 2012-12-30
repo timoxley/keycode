b252d58f693eddf6a356f39110fe917cf75712d7
jkroso <jkroso@gmail.com>
2012-12-30 17:48:10 +1300

    more efficient

--- test/keycode.js
+++ test/keycode.js
@@ -1,10 +1,10 @@
 var keycode = require('keycode')
 var assert = require('timoxley-assert')
 it('is commutative', function() {
-  var count = 0
-  for (var key in keycode.keyNames) {
-    assert.strictEqual(keycode(key), keycode(keycode(keycode(key))))
-    count++
-  }
-  console.debug('Tested %d keys', count)
+	var count = 0
+	for (var key in keycode.code) {
+		assert.strictEqual(keycode(key), keycode(keycode(keycode(key))))
+		count++
+	}
+	console.debug('Tested %d keys', count)
 })
