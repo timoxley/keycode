1b88ec5b074b9f1e28b7eb853b596ecc8edfa3f9
Amir Abu Shareb <amir.abu.shareb@me.com>
2013-01-30 10:25:58 +0200

    fix styling

--- index.js
+++ index.js
@@ -9,12 +9,12 @@
  * @api public
  */
 exports = module.exports = function (search) {
-	if ('string' == typeof search) {
-		return codes[search.toLowerCase()]
-			|| search.toUpperCase().charCodeAt(0);
-	} else {
-		return names[search];
-	}
+  if ('string' == typeof search) {
+    return codes[search.toLowerCase()]
+      || search.toUpperCase().charCodeAt(0);
+  } else {
+    return names[search];
+  }
 }
 
 /**
@@ -23,48 +23,48 @@ exports = module.exports = function (search) {
  *   exports.code['Enter'] // => 13
  */
 var codes = exports.code = {
-	'backspace': 8,
-	'tab': 9,
-	'enter': 13,
-	'shift': 16,
-	'ctrl': 17,
-	'alt': 18,
-	'pause/break': 19,
-	'caps lock': 20,
-	'esc': 27,
-	'space': 32,
-	'page up': 33,
-	'page down': 34,
-	'end': 35,
-	'home': 36,
-	'left': 37,
-	'up': 38,
-	'right': 39,
-	'down': 40,
-	'insert': 45,
-	'delete': 46,
-	'windows': 91,
-	'right click': 93,
-	'numpad *': 106,
-	'numpad +': 107,
-	'numpad -': 109,
-	'numpad .': 110,
-	'numpad /': 111,
-	'num lock': 144,
-	'scroll lock': 145,
-	'my computer': 182,
-	'my calculator': 183,
-	';': 186,
-	'=': 187,
-	',': 188,
-	'-': 189,
-	'.': 190,
-	'/': 191,
-	'`': 192,
-	'[': 219,
-	'\\': 220,
-	']': 221,
-	"'": 222
+  'backspace': 8,
+  'tab': 9,
+  'enter': 13,
+  'shift': 16,
+  'ctrl': 17,
+  'alt': 18,
+  'pause/break': 19,
+  'caps lock': 20,
+  'esc': 27,
+  'space': 32,
+  'page up': 33,
+  'page down': 34,
+  'end': 35,
+  'home': 36,
+  'left': 37,
+  'up': 38,
+  'right': 39,
+  'down': 40,
+  'insert': 45,
+  'delete': 46,
+  'windows': 91,
+  'right click': 93,
+  'numpad *': 106,
+  'numpad +': 107,
+  'numpad -': 109,
+  'numpad .': 110,
+  'numpad /': 111,
+  'num lock': 144,
+  'scroll lock': 145,
+  'my computer': 182,
+  'my calculator': 183,
+  ';': 186,
+  '=': 187,
+  ',': 188,
+  '-': 189,
+  '.': 190,
+  '/': 191,
+  '`': 192,
+  '[': 219,
+  '\\': 220,
+  ']': 221,
+  "'": 222
 }
 
 /*!
